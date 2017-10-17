8f51e43045a07ba479a3616c0a09ee17b3ec8356
Pzhe <zheair923@gmail.com>
2017-10-18 02:58:48 +0800

    Improve preprocessing algorithm

--- ocrdemo.py
+++ ocrdemo.py
@@ -46,21 +46,48 @@ def image2text(filename):
     print(ocr_word)
     return ocr_word
 
+def smoothing(img):
+    blur = cv2.GaussianBlur(img, (1, 1), 0)
+    th = cv2.threshold(blur, 140, 255, cv2.THRESH_BINARY)[1]
+    return th
+
 def image_process(filename):
     output_filename='process.jpg'
-    img = cv2.imread(filename)
-    ref1 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    ref2 = cv2.threshold(ref1, 100, 255, cv2.THRESH_BINARY_INV)[1]
-    ref3 = cv2.dilate(ref2, (2, 2), iterations=1)
-    titles = ['Original Image', 'BGR2GRAY', 'threshold', 'dilate']
-    images = [img, ref1, ref2, ref3]
+    img = cv2.imread(filename,0)
+    ref1 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 47, 5)
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(1,1))
+    opening = cv2.morphologyEx(ref1, cv2.MORPH_OPEN,kernel) # removing noise
+    closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE,kernel) # closing small holes
+    ref2 = smoothing(img)
+    ref3 = cv2.bitwise_or(closing, ref2)
+    ref4 = cv2.dilate(ref3,kernel,iterations=1)
+    titles = ['adaptiveThreshold', 'open-closing', 'smoothing', 'OR-dilate']
+    images = [ref1, closing, ref2, ref4]
     # for i in xrange(4):
     #     plt.subplot(2,2,i+1),plt.imshow(images[i],'gray')
     #     plt.title(titles[i])
     #     plt.xticks([]),plt.yticks([])
     # plt.show()
-    cv2.imwrite(output_filename,ref3)
+    cv2.imwrite(output_filename,ref4)
     return output_filename
+
+# Orignal method
+# def image_process(filename):
+#     output_filename='process.jpg'
+#     img = cv2.imread(filename)
+#     ref1 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#     ref2 = cv2.threshold(ref1, 100, 255, cv2.THRESH_BINARY_INV)[1]
+#     ref3 = cv2.dilate(ref2, (2, 2), iterations=1)
+#     titles = ['Original Image', 'BGR2GRAY', 'threshold', 'dilate']
+#     images = [img, ref1, ref2, ref3]
+#     for i in xrange(4):
+#         plt.subplot(2,2,i+1),plt.imshow(images[i],'gray')
+#         plt.title(titles[i])
+#         plt.xticks([]),plt.yticks([])
+#     plt.show()
+#     plt.imshow(ref3,'gray')
+#     cv2.imwrite(output_filename,ref3)
+#     return output_filename
 
 if __name__ == '__main__':
     out=image_process('testimg.jpg')
